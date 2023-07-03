beb42b62016e29b5e5307060e31854771e4f6e5c
Jerry Tsai <jerry.nt.tsai@gmail.com>
2023-07-03 16:46:45 -0400

    version 13

--- main.js
+++ main.js
@@ -2,3 +2,4 @@ console.log('JS added');
 console.log('JS changed');
 console.log('version 11');
 console.log('version12');
+console.log('version 13');
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 console.log('change');
 console.log('version12');
+console.log('version 13');
